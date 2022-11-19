9763241564f3e1189844bc55202c8bc656a0191e
kyljmeeskifk <amir.syrgabaev000@gmail.com>
2022-11-19 11:31:39 +0600

    one

--- src/main/java/one/library/frontend/view/MainView.java
+++ src/main/java/one/library/frontend/view/MainView.java
@@ -1,37 +1,11 @@
 package one.library.frontend.view;
 
-import com.vaadin.componentfactory.Autocomplete;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-import one.library.backend.repository.AuthorRepository;
-import one.library.backend.service.AuthorService;
-
-import java.util.ArrayList;
 
 @Route("")
 public class MainView extends VerticalLayout {
 
-    private final AuthorService authorService;
-    TextField titleTextField = new TextField("Title");
-    Autocomplete authorTextField;
 
-    public MainView(AuthorRepository authorRepository, AuthorService authorService) {
-        this.authorService = authorService;
-
-        configureAuthorTextField();
-
-        add(titleTextField, authorTextField);
-    }
-
-    private void configureAuthorTextField() {
-        authorTextField = new Autocomplete(5);
-        authorTextField.setLabel("Author");
-        authorTextField.addChangeListener(event -> {
-            authorTextField.setOptions(authorService.getAuthorNames(event.getValue()));
-        });
-    }
 
 }
